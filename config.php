43225c700371ce15e771313a1f4db035fa03401d
HMS-Victory <banrabas.sacket@gmail.com>
2023-04-27 09:06:40 -0600

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Configuration settings
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cma');
+define('DB_USER', 'cmswww');
+define('DB_PASS', 'e34tbBzSqkcj@hB8');
+
+// email stuff
+// define('SMTP_HOST', 'mail.example.com');
+// define('SMTP_USER', 'user@example.com');
+// define('SMTP_PASS', 'secret');
+
+define('SHOW_ERROR_DETAIL', true);
